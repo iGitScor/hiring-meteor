6cb4c6a09ccef658883cf3917b65c6509b1a94f1
mathieu-integre-mac <mathieu@1001pharmacies.com>
2016-03-10 17:03:00 +0100

    [UP] step 7

--- client/job-details/job-details.ts
+++ client/job-details/job-details.ts
@@ -4,19 +4,31 @@ import {RouteParams} from 'angular2/router';
 
 import {Jobs} from 'collections/jobs';
 
+import {RouterLink} from 'angular2/router';
 
 @Component({
   selector: 'job-details'
 })
 @View({
-  templateUrl: '/client/job-details/job-details.html'
+  templateUrl: '/client/job-details/job-details.html',
+  directives: [RouterLink]
 })
 
 export class JobDetails {
-    job: Object;
+    job: Job;
 
     constructor(params: RouteParams) {
         var jobId = params.get('jobId');
         this.job = Jobs.findOne(jobId);
+    }
+
+     saveJob(job) {
+      Jobs.update(job._id, {
+        $set: {
+          name: job.name,
+          description: job.description,
+          location: job.location
+        }
+      });
     }
 }
