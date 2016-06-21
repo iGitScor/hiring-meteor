f1068d704a71339199e07ea5560d6d88f86dd826
iGitScor <sebastien.correaud@gmail.com>
2016-06-21 05:49:17 -0700

    Change ts compiler configuration

---
+++ typings/browser.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="browser/ambient/es6-promise/es6-promise.d.ts" />
+/// <reference path="browser/ambient/es6-shim/es6-shim.d.ts" />
+/// <reference path="browser/ambient/meteor/meteor.d.ts" />
